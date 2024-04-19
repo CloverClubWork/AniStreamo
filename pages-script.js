b1bb6d3ee15cddd7b0d31ca94b0a9790f3744fc1
CloverClub03 <114269541+CloverClubWork@users.noreply.github.com>
2024-04-19 08:37:25 +0800

    Add files via upload

--- pages-script.js
+++ pages-script.js
@@ -61,6 +61,24 @@ $(document).ready(function() {
 
         splitEpisode();
         
+        $('#share-btn').click(function() {
+          if (navigator.share) {
+            navigator.share({
+                title: document.title,
+                text: document.querySelector('meta[name="description"]').getAttribute('content'),
+                url: window.location.href
+              }).then(() => {
+                console.log('Successfully shared');
+              })
+              .catch((error) => {
+                console.log('Error sharing', error);
+              });
+          } else {
+            // Fallback for browsers that do not support the Web Share API
+            alert('Web Share API is not supported in your browser.');
+          }
+        });
+        
         // Comments
         var disqus_config = function() {
           this.page.url = window.location.href; // Replace PAGE_URL with your page's canonical URL variable
